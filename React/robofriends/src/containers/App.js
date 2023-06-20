9403e18d2e55cebea84fcfc41b9d349a6338d06a
mehulgdoshi <32913930+mehulgdoshi@users.noreply.github.com>
2023-06-19 21:36:58 -0400

    fixing errors

--- React/robofriends/src/containers/App.js
+++ React/robofriends/src/containers/App.js
@@ -1,10 +1,9 @@
-import React, { Component } from "react";
-import CardList from "../components/CardList";
-//import { robots } from './robots';
+import React, { Component } from 'react';
+import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll';
-import ErrorBoundry from "../components/ErrorBoundry";
+import ErrorBoundry from '../components/ErrorBoundry';
 
 
 class App extends Component {
